c1f6fd3f0491d7bf0bdc3fb5911bf25f9ab9c84d
TalipSalihoglu <59333488+TalipSalihoglu@users.noreply.github.com>
2020-11-24 21:59:56 +0300

    Admin Area eklendi

---
+++ ZSözlük/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZSözlük.Repository;
+
+namespace ZSözlük.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        private readonly IcerikRepository _ıcerikRepository;
+        public AdminController(IcerikRepository icerikRepository)
+        {
+            _ıcerikRepository = icerikRepository;
+        }
+        public async Task<IActionResult> Index()
+        {         
+            return View(await _ıcerikRepository.GetirİcerikHepsi());
+        }
+    }
+}
