5ee7555601d9d3a823f8fbbda32628e7dbec3325
TalipSalihoglu <59333488+TalipSalihoglu@users.noreply.github.com>
2020-12-18 16:00:01 +0300

    Update applicationUser

--- ZSözlük/Models/ApplicationUser.cs
+++ ZSözlük/Models/ApplicationUser.cs
@@ -12,7 +12,9 @@ namespace ZSözlük.Models
         public string FirstName{ get; set; }
         public string LastName{ get; set; }
         public DateTime? DateOfBirth{ get; set; }
-
+        public int Age{ get; set; }
+        public string Country{ get; set; }
+        public string City{ get; set; }
         public List<Icerik> Icerikler { get; set; }
 
     }
