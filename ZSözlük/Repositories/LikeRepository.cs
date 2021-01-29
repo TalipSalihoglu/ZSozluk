1ee6853bad05ef6ff9a8d2d5ef95769f3d9113d4
TalipSalihoglu <59333488+TalipSalihoglu@users.noreply.github.com>
2021-01-29 22:58:20 +0300

    added IlikeRepository and likeRepository

---
+++ ZSözlük/Repositories/LikeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZSözlük.Contexts;
+using ZSözlük.IRepositories;
+using ZSözlük.Models;
+
+namespace ZSözlük.Repositories
+{
+    public class LikeRepository : Repository<LikeModel>, ILikeRepository
+    {
+        public LikeRepository(ZSozlukContext zSozlukContext) : base(zSozlukContext)
+        {
+
+        }
+    }
+}
