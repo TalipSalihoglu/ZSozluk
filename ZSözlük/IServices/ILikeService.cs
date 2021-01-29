0c29938bdad0036515f7c5475eef354d88ace4b4
TalipSalihoglu <59333488+TalipSalihoglu@users.noreply.github.com>
2021-01-29 22:58:54 +0300

    added ILikeService and LikeService

---
+++ ZSözlük/IServices/ILikeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZSözlük.IServices
+{
+    public interface ILikeService
+    {
+        Task<int> Like(int id, string _userid);
+        public Task<int> CancelLike(int id, string _userid);
+        public bool isLike(int id, string _userid);
+        public int CountOfLike(int id);
+    }
+}
---
+++ ZSözlük/Services/LikeService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZSözlük.Contexts;
+using ZSözlük.IRepositories;
+using ZSözlük.IServices;
+using ZSözlük.Models;
+
+namespace ZSözlük.Services
+{
+    public class LikeService : ILikeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LikeService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<int> Like(int id, string _userid)
+        {
+            LikeModel m = new LikeModel();
+            m.IcerikID = id;
+            m.UserID = _userid;
+            await _unitOfWork.Likes.AddAsync(m);
+            await _unitOfWork.CommitAsync();
+            return id;
+        }
+        public async Task<int> CancelLike(int id, string _userid)
+        {
+            var kk = _unitOfWork.Likes.GetAllAsync().Result.Where(x => x.IcerikID == id && x.UserID == _userid).FirstOrDefault();
+            _unitOfWork.Likes.Remove(kk);
+            await _unitOfWork.CommitAsync();
+            return id;
+        }
+
+        public int CountOfLike(int id)
+        {
+            return _unitOfWork.Likes.GetAllAsync().Result.Where(x => x.IcerikID == id).Count();
+        }
+
+        public bool isLike(int id, string _userid)
+        {
+            var urun = _unitOfWork.Icerikler.GetByIdAsync(id);
+            var userid = _userid;
+            if (_unitOfWork.Likes.GetAllAsync().Result.Where(x => x.UserID == userid && x.IcerikID == id).Count() == 0)
+                return false;
+            return true;
+        }
+
+       
+    }
+}
